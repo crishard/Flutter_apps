03450f08832dc44686b91590edd330a301e6a5cf
crishard <crislantorres9@gmail.com>
2022-05-20 20:39:03 -0300

    removendo appbar e adicionando font

--- appdesign/lib/main.dart
+++ appdesign/lib/main.dart
@@ -26,8 +26,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
       body: Center(
       ),
     );
