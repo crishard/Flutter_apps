3d53b518807975d0c65271a62bb498f0d094122f
crishard <crislantorres9@gmail.com>
2022-05-23 21:23:59 -0300

    card ganhos valor

--- desafio_layout/lib/models/components/ganhos_card.dart
+++ desafio_layout/lib/models/components/ganhos_card.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class GanhosCard extends StatelessWidget{
+  const GanhosCard({ Key? key }) : super(key: key);
+
+  @override
+  Widget build( BuildContext context){
+
+    num _ganhos = 1200;
+
+    return Container(
+      
+    );
+  }
+}
