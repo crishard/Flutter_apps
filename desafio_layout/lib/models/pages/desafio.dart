2dc6b5f3205e0916a039c9128b1cde5f1d7b31a5
crishard <crislantorres9@gmail.com>
2022-05-27 14:37:13 -0300

    criando themes

---
+++ desafio_layout/lib/models/pages/desafio.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:desafio_layout/models/components/cards/notificacoes.dart';
+import 'package:desafio_layout/models/components/cards/ganhos.dart';
+
+class MyApp extends StatelessWidget{
+
+  const MyApp ({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Desafio Layout',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      home:  DesafioLayout(),
+    );
+  }
+}
+
+class DesafioLayout extends StatefulWidget {
+  const DesafioLayout ({ Key? key }) : super(key: key);
+
+  @override
+  State<DesafioLayout> createState() => _DesafioLayoutState();
+}
+
+class _DesafioLayoutState extends State<DesafioLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black38,
+
+      body: Stack(
+       children: [
+         SingleChildScrollView(
+           child: Padding(
+             padding: const EdgeInsets.only(top: 35, left: 35, right: 35),
+            child: Column(
+             children: [
+               Padding(padding: const EdgeInsets.only(top: 20, bottom: 55),
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const CircleAvatar(
+                     radius: 40,
+                     backgroundImage: AssetImage('assets/images/perfil.jpeg'),
+                   ),
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.end,
+                     children: const [
+                       Text('Olá', style: TextStyle(fontSize: 20, color: Colors.white),),
+                       Text('João!', style: TextStyle(fontSize: 50, color: Colors.white),),
+                     ],
+                   )
+                 ],
+               ),
+               ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Parabéns! Esse mês você fez",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Icon(
+                        Icons.remove_red_eye_rounded, 
+                        color: Colors.white, 
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                  NotificacoesCard(),
+                  GanhosCard(),
+             ],
+            ),   
+          ),
+         ),
+        //  MyStatefulWidget(),
+      // Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: [
+      //   Icon(Icons.menu, color: Colors.purple, size: 30,),
+      // ],)
+       ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // backgroundColor: Colors.black38,
+        items: const[
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.purple,),
+            label: 'Home',
+            backgroundColor: Colors.black38,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop_two,),
+            label: 'Loja',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt),
+            label: 'Pessoas',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.escalator),
+            label: 'Informações',
+            backgroundColor: Colors.purple,
+          ),
+        ],
+    ),
+    );
+  }
+}
