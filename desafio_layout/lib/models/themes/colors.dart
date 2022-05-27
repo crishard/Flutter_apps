269ac22c3a4c98188f3ae2c3347faabdcb2a9eed
crishard <crislantorres9@gmail.com>
2022-05-27 14:46:08 -0300

    criando colors para thema white e dark

--- desafio_layout/lib/models/themes/colors.dart
+++ desafio_layout/lib/models/themes/colors.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class ComponetsColor {
+
+  // Colors for the theme white 
+  static const PrimeryColorWhite = Color.fromRGBO(230, 230, 230, 10);
+
+  // Colors for the theme black
+  static const PrimeryColorBlack = Color.fromRGBO(38, 38, 38, 10);
+}
