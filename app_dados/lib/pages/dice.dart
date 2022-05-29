f4a109de04a8c58f9958724a9b59456c677051db
crishard <crislantorres9@gmail.com>
2022-05-29 20:12:31 -0300

    criando class dicepage e dicepagestate

--- app_dados/lib/pages/dice.dart
+++ app_dados/lib/pages/dice.dart
@@ -17,5 +17,20 @@ class MyApp extends StatelessWidget{
 class DicePage extends StatefulWidget{
   const DicePage({Key ? key}) : super(key: key);
   @override
-  _DicePageState createState() => _DicePageState();
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dice'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
 }
