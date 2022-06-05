a23566dcaeb0d32d7a1aad82ab048fc392146fa8
crishard <crislantorres9@gmail.com>
2022-06-05 03:51:53 -0300

    configurando quadro

--- jogo_da_velha/lib/widgets/board.dart
+++ jogo_da_velha/lib/widgets/board.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+
+class Board {
+  String simbolo;
+  bool enable;
+  final int id;
+  Color color;
+
+  Board(this.id,{
+    this.simbolo = "",
+    this.enable = true,
+    this.color = Colors.white,
+  });
+}
