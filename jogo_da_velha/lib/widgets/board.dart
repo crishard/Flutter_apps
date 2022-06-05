1972a0e62a4bf374bef84bc4c93973537d930bd6
crishard <crislantorres9@gmail.com>
2022-06-05 04:41:10 -0300

    completando regras de jogo

--- jogo_da_velha/lib/widgets/board.dart
+++ jogo_da_velha/lib/widgets/board.dart
@@ -5,11 +5,9 @@ class Board {
   String simbolo;
   bool enable;
   final int id;
-  Color color;
 
   Board(this.id,{
     this.simbolo = "",
     this.enable = true,
-    this.color = Colors.white,
   });
 }
