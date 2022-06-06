6d4b1138c35d8e585230b790d7e964b0f7c650f6
crishard <crislantorres9@gmail.com>
2022-06-06 20:14:32 -0300

    construtores da class dialogo

--- jogo_da_velha/lib/models/dialogo.dart
+++ jogo_da_velha/lib/models/dialogo.dart
@@ -2,5 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:jogo_da_velha/jogo/contants.dart';
 
 class Dialogo extends StatelessWidget{
-  
+  final String mensagem;
+  final String titulo;
+  final Function onPressed;
+
+
+  const Dialogo({required this.mensagem, required this.titulo, required this.onPressed});
+
+
 }
