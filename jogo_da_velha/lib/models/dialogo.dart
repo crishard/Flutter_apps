be4ef031acf1933a9b76d30ed0ee4226637877c5
crishard <crislantorres9@gmail.com>
2022-06-06 20:19:19 -0300

    class dialogo completa

--- jogo_da_velha/lib/models/dialogo.dart
+++ jogo_da_velha/lib/models/dialogo.dart
@@ -6,16 +6,22 @@ class Dialogo extends StatelessWidget{
   final String titulo;
   final Function onPressed;
 
-
   const Dialogo({required this.mensagem, required this.titulo, required this.onPressed});
-
 
   @override
   Widget build(BuildContext context){
     return AlertDialog(
-      
+      title: Text(titulo),
+      content: Text(mensagem),
+      actions: [
+        FlatButton(
+          child: Text(restart),
+          onPressed: (){
+            Navigator.pop(context);
+            onPressed();
+          },
+        ),
+      ],
     );
   }
-
-
 }
